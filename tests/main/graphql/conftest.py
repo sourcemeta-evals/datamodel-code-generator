1253a7bf199a43917a1050b2524ca8397d987e53
Juan Cruz Viotti <jv@jviotti.com>
2026-02-13 13:41:25 -0400

    Delete other tests
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- tests/main/graphql/conftest.py
+++
@@ -1,8 +0,0 @@
-"""Shared fixtures for GraphQL tests."""
-
-from __future__ import annotations
-
-from tests.conftest import create_assert_file_content
-from tests.main.conftest import EXPECTED_GRAPHQL_PATH
-
-assert_file_content = create_assert_file_content(EXPECTED_GRAPHQL_PATH)
